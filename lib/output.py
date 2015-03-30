301b05b09db8e121893ac4e4a85755ad99845c44
András Veres-Szentkirályi <vsza@vsza.hu>
2015-03-30 14:28:59 +0200

    added OR instruction decoding

--- lib/output.py
+++ lib/output.py
@@ -28,7 +28,8 @@ from capstone.x86 import (X86_INS_ADD, X86_INS_AND, X86_INS_CMP, X86_INS_DEC,
         X86_INS_IDIV, X86_INS_IMUL, X86_INS_INC, X86_INS_MOV, X86_INS_SHL,
         X86_INS_SHR, X86_INS_SUB, X86_INS_XOR, X86_OP_FP, X86_OP_IMM,
         X86_OP_INVALID, X86_OP_MEM, X86_OP_REG, X86_REG_EBP, X86_REG_EIP,
-        X86_REG_RBP, X86_REG_RIP, X86_INS_CDQE, X86_INS_LEA, X86_INS_MOVSX)
+        X86_REG_RBP, X86_REG_RIP, X86_INS_CDQE, X86_INS_LEA, X86_INS_MOVSX,
+        X86_INS_OR)
 
 
 binary = None
@@ -267,7 +268,7 @@ def print_inst(i, tab=0, prefix=""):
 
     inst_check = [X86_INS_SUB, X86_INS_ADD, X86_INS_MOV, X86_INS_CMP,
             X86_INS_XOR, X86_INS_AND, X86_INS_SHR, X86_INS_SHL, X86_INS_IMUL,
-            X86_INS_DEC, X86_INS_INC, X86_INS_LEA, X86_INS_MOVSX]
+            X86_INS_DEC, X86_INS_INC, X86_INS_LEA, X86_INS_MOVSX, X86_INS_OR]
 
     if i.id in inst_check:
         print_operand(i, 0)
