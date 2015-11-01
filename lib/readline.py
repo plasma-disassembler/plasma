81e9235a48038b161bae2801c50189a4255c0c1c
joelpx <joel@hippersoft.fr>
2015-11-01 18:36:00 +0100

    interactive: fix: move cursor after auto-completion

--- lib/readline.py
+++ lib/readline.py
@@ -426,8 +426,9 @@ class ReadLine():
             self.cursor_j = 0
             self.set_cursor()
             self.delete_end_line()
-            self.cursor_j = len(self.line)
             self.print(self.line)
+            self.cursor_j = len(completed)
+            self.set_cursor()
 
     def k_ctrl_a(self):
         self.k_home()
