4a18a5fa6027999ce2372a9e45ec61f04004920f
joelpx <joel@hippersoft.fr>
2016-02-06 22:41:18 +0100

    deref only if it's a symbol or defined as code
    With the command "I" in the visual mode, address are not dereferenced.

--- reverse/lib/arch/mips/output.py
+++ reverse/lib/arch/mips/output.py
@@ -98,7 +98,9 @@ class Output(OutputAbs):
                     sz = self.get_offset_size(ad)
                     if sz != -1:
                         val = section.read_int(ad, sz)
-                        if val in self.gctx.db.reverse_symbols:
+                        if self.gctx.capstone_string == 0 and \
+                                (self.gctx.dis.mem.is_code(val) or \
+                                 self.gctx.db.reverse_symbols):
                             self._imm(val, 0, True, print_data=False,
                                       force_dont_print_data=force_dont_print_data)
                             return True
